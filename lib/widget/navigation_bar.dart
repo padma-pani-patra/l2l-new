18e666dd7f72a08216720b6dc20dc57ed3fe8ef3
padma-pani-patra <padmapanipatra@gmail.com>
2021-02-23 12:26:01 +0530

    initial commit

--- lib/widget/navigation_bar.dart
+++ lib/widget/navigation_bar.dart
@@ -22,61 +22,91 @@ class _NavBarState extends State<NavBar> {
           items: [
             BottomNavigationBarItem(
               icon: Container(
-                height: 30,
-                width: 30,
-                child: Image.asset(
-                  'assets/icons/Group_526.png',
-                  height: 27,
-                  width: 29,
-                ),
+                height: widget.activeIndex == 0 ? 45 : 30,
+                width: widget.activeIndex == 0 ? 45 : 30,
+                child: widget.activeIndex == 0
+                    ? Image.asset(
+                        'assets/icons/Group 533.png',
+                        height: 27,
+                        width: 29,
+                      )
+                    : Image.asset(
+                        'assets/icons/Group_526.png',
+                        height: 27,
+                        width: 29,
+                      ),
               ),
               label: "L2L Wall",
             ),
             BottomNavigationBarItem(
               icon: Container(
-                height: 45,
-                width: 45,
-                child: Image.asset(
-                  'assets/icons/Group 532.png',
-                  height: 23.2,
-                  width: 23.2,
-                ),
+                height: widget.activeIndex == 1 ? 45 : 30,
+                width: widget.activeIndex == 1 ? 45 : 30,
+                child: widget.activeIndex == 1
+                    ? Image.asset(
+                        'assets/icons/Group 532.png',
+                        height: 27,
+                        width: 29,
+                      )
+                    : Image.asset(
+                        'assets/icons/Group 527.png',
+                        height: 27,
+                        width: 29,
+                      ),
               ),
               label: "My Lesson",
             ),
             BottomNavigationBarItem(
               icon: Container(
-                height: 30,
-                width: 30,
-                child: Image.asset(
-                  'assets/icons/Group 530.png',
-                  height: 27,
-                  width: 29,
-                ),
+                height: widget.activeIndex == 2 ? 45 : 30,
+                width: widget.activeIndex == 2 ? 45 : 30,
+                child: widget.activeIndex == 2
+                    ? Image.asset(
+                        'assets/icons/Group 531.png',
+                        height: 27,
+                        width: 29,
+                      )
+                    : Image.asset(
+                        'assets/icons/Group 530.png',
+                        height: 27,
+                        width: 29,
+                      ),
               ),
               label: "L2L Feed",
             ),
             BottomNavigationBarItem(
               icon: Container(
-                height: 30,
-                width: 30,
-                child: Image.asset(
-                  'assets/icons/Group 528.png',
-                  height: 27,
-                  width: 29,
-                ),
+                height: widget.activeIndex == 3 ? 45 : 30,
+                width: widget.activeIndex == 3 ? 45 : 30,
+                child: widget.activeIndex == 3
+                    ? Image.asset(
+                        'assets/icons/Group 534.png',
+                        height: 27,
+                        width: 29,
+                      )
+                    : Image.asset(
+                        'assets/icons/Group 528.png',
+                        height: 27,
+                        width: 29,
+                      ),
               ),
               label: "My Lessons",
             ),
             BottomNavigationBarItem(
               icon: Container(
-                height: 30,
-                width: 30,
-                child: Image.asset(
-                  'assets/icons/Group 529.png',
-                  height: 27,
-                  width: 29,
-                ),
+                height: widget.activeIndex == 4 ? 45 : 30,
+                width: widget.activeIndex == 4 ? 45 : 30,
+                child: widget.activeIndex == 4
+                    ? Image.asset(
+                        'assets/icons/Group 535.png',
+                        height: 27,
+                        width: 29,
+                      )
+                    : Image.asset(
+                        'assets/icons/Group 529.png',
+                        height: 27,
+                        width: 29,
+                      ),
               ),
               label: "My Account",
             )
